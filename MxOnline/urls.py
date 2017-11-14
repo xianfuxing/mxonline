c2d9f7b667e6d69fe424b8aa78710ff349aa0ef5
xianfuxing <xianfuxing@126.com>
2017-11-14 23:49:23 +0800

    # add login

--- MxOnline/urls.py
+++ MxOnline/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     # url(r'^admin/', admin.site.urls),
     url(r'^xadmin/', xadmin.site.urls),
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
+    url(r'^login/$', TemplateView.as_view(template_name='login.html'), name='login'),
 ] +  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
