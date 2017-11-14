632b2782c09ae3694f7550aa08b79c1f208b8023
xianfuxing <xianfuxing@126.com>
2017-11-14 23:40:49 +0800

    # add static/media

--- MxOnline/settings.py
+++ MxOnline/settings.py
@@ -133,3 +133,10 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- MxOnline/urls.py
+++ MxOnline/urls.py
@@ -14,11 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 # from django.contrib import admin
+from django.views.generic import TemplateView
 import xadmin
 
 
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
     url(r'^xadmin/', xadmin.site.urls),
-]
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
+] +  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
