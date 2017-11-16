69da9586fe5fadbba074a7402a4c4bee836bca57
xianfuxing <xianfuxing@126.com>
2017-11-17 00:38:47 +0800

    # update login

--- MxOnline/urls.py
+++ MxOnline/urls.py
@@ -18,6 +18,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 # from django.contrib import admin
 from django.views.generic import TemplateView
+from users.views import user_login
 import xadmin
 
 
@@ -25,5 +26,5 @@ urlpatterns = [
     # url(r'^admin/', admin.site.urls),
     url(r'^xadmin/', xadmin.site.urls),
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
-    url(r'^login/$', TemplateView.as_view(template_name='login.html'), name='login'),
+    url(r'^login/$', user_login, name='login'),
 ] +  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        user = authenticate(username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('index')
+
+    return render(request, 'login.html')
