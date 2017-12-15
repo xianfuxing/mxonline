7b9d914ae8fb43238ffda170f21f8e19c6cac177
xianfuxing <xianfuxing@126.com>
2017-12-16 00:01:19 +0800

    # update is_active_reverse

--- apps/organization/templatetags/common_tags.py
+++ apps/organization/templatetags/common_tags.py
@@ -14,3 +14,14 @@ def is_active(request, *url_strings):
         if urlpattern in request.path:
             return 'active2'
         return ''
+
+
+@register.simple_tag()
+def is_active_reverse(request, args, *urlnames):
+    for urlname in urlnames:
+        if not args:
+            args = None
+        url = reverse(urlname, args=[args])
+        if url in request.path:
+            return 'active2'
+        return ''
