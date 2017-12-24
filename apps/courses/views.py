fd46afb1722b83d5536f5b64fa46131809f10ece
xianfuxing <xianfuxing@126.com>
2017-12-24 22:46:38 +0800

    # add course list

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import View
 
+from .models import Course
+
 
 class CourseListView(View):
     def get(self, request):
-        return render(request, 'courses/course-list.html')
+        courses = Course.objects.all()
+        context = {
+            'courses': courses
+        }
+        return render(request, 'courses/course-list.html', context=context)
