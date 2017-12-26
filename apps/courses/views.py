b9ddefd7171cc1fdc6ada024b5710ccdfefea479
xianfuxing <xianfuxing@126.com>
2017-12-26 23:39:17 +0800

    # add cousrse list pagination

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import View
+from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Course
 
@@ -7,6 +8,16 @@ from .models import Course
 class CourseListView(View):
     def get(self, request):
         courses = Course.objects.all()
+
+        page = request.GET.get('page', 1)
+        p = Paginator(courses, 3, request=request)
+
+        try:
+            courses = p.page(page)
+        except PageNotAnInteger:
+            courses = p.page(1)
+
+
         context = {
             'courses': courses
         }
